b04bd1736c754aec09974d6139801dce62873751
Emir Hasan Işık <emir.h.isik@gmail.com>
2021-09-22 21:28:05 +0300

    Change Title

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Panel Based by JSON</title>
     <style type="text/css">
     * {
         padding: 0;
